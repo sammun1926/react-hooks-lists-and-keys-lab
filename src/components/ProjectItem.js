a9d266a282548d26e2657f79fde565c855fa40b6
sammun1926 <munorusam10@gmail.com>
2023-04-23 03:51:27 +0300

    completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,4 +1,6 @@
+import { render } from "@testing-library/react";
 import React from "react";
+import ProjectList from "./ProjectList";
 
 function ProjectItem({ name, about, technologies }) {
   return (
@@ -7,6 +9,15 @@ function ProjectItem({ name, about, technologies }) {
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        
+        <span>Rails</span>
+
+        <span>Bootstrap CSS</span>
+
+        <span>React</span>
+
+        <span>redux</span>
+
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,14 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        
+        
+        <ProjectItem id={1} name={"Reciplease"} about= {"A recipe tracking app"} technologies={["Rails", "Bootstrap CSS"]}></ProjectItem>
+
+        <ProjectItem id={2} name={"Kibbles N Bitz"} about= {"Tinder for dogs"} technologies={["React", "Redux"]}></ProjectItem>
+
+        <ProjectItem id={3} name={"Alienwares"} about= {"Etsy for aliens"} technologies={["React", "Redux", "Rails"]}></ProjectItem></div>
     </div>
   );
 }
